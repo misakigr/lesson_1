17e2e9148101770127268d9950a5f7b619acd49a
AdminAraks <misakigr@mail.ru>
2022-07-13 21:20:16 +0300

    Problem7.py

---
+++ Problem7.py
@@ -0,0 +1,12 @@
+n = int(input("Введите верхнюю границу диапазона: "))
+b = int(input("Введите мах порядковый номер желаемого числа: "))
+sieve = set(range(2, n+1))
+c = 1
+while sieve:
+    prime = min(sieve)
+    #print(prime, end = "\t")
+    print(c, prime)
+    sieve -= set(range(prime, n+1, prime))
+    if c == b:
+        break
+    c += 1
