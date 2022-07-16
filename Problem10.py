cb92945372e71d5735b722f6b6e8a5b00c18e969
misakigr <misakigr@mail.ru>
2022-07-16 17:51:58 +0300

    p10

--- Problem10.py
+++ Problem10.py
@@ -10,12 +10,19 @@
 #     summ = summ + prime
 # print(f'Сумма всех простых чисел ниже {n} равна {summ}')
 
-def eratosthen(n):
+
+def main():
     sieve = list(range(n))
     sieve[1] = 0
     for i in sieve[2:]:
-            for j in range(i + i, len(sieve), i):
-                sieve[j] = 0
+        for j in range(i ** 2, len(sieve), i):
+            sieve[j] = 0
     return sieve
-n = 2_000_000
-print(f'Сумма всех простых чисел ниже {n} равна {sum(eratosthen(n))}')
+
+
+if __name__ == '__main__':
+    n = 2_000_000
+    result = str(sum(main()))
+    res = result[-12:-9] + ' ' + result[-9:-6] + ' ' + result[-6:-3] + ' ' + result[-3:]
+    print(f'Сумма всех простых чисел ниже {n}'
+          f' равна: {res}')
