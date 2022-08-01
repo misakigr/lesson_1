2140155530cede3a9d1cdc54e7ea38ddb5edcd0f
AdminAraks <misakigr@mail.ru>
2022-08-01 22:01:10 +0300

    Problem12.py

--- Problem12.py
+++ Problem12.py
@@ -1,23 +1,16 @@
-# k = 0
-# summ = 0
-#
-# while k < 10:
-#     razl = []
-#     k += 1
-#     for i in range(k):
-#         summ += 1
-#     for j in range(len(str(summ))+1):
-#         if j > 0:
-#             razl.append(summ / j)
-#     print(summ, ":", razl)
+j= 1
+n=0
+while 1:
+    n = j + n
+    j = j + 1
+    print(n, end="\r")
 
-n = 1
-while n < 10:
-    n += n
     razl = []
     k = 0
     for i in range(n + 1):
         if i > 0 and n % i == 0:
             razl.append(int(n / (n / i)))
             k += 1
-    print(n, "(",k,"делител. )", ":", razl)
+    if k >= 37:
+        print(n, "(",k,"делител. )", ":", razl)
+        break
--- Problem7.py
+++ Problem7.py
@@ -1,13 +1,27 @@
+
+# while sieve:
+#     prime = min(sieve)
+#     #print(prime, end = "\t")
+#     print(c, prime)
+#     sieve -= set(range(prime, n+1, prime))
+#     if c == b:
+#         break
+#     c += 1
+
+c = 1
+nacealo = 2
 n = int(input("Введите верхнюю границу диапазона: "))
 b = int(input("Введите мах порядковый номер желаемого числа: "))
-sieve = set(range(2, n+1))
-c = 1
-while sieve:
-    prime = min(sieve)
-    #print(prime, end = "\t")
-    print(c, prime)
-    sieve -= set(range(prime, n+1, prime))
-    if c == b:
-        break
-    c += 1
+sieve = set(range(nacealo, n+1))
 
+def sie(sieve, c):
+    while sieve:
+        prime = min(sieve)
+        #print(prime, end = "\t")
+        print(c, prime)
+        sieve -= set(range(prime, n+1, prime))
+        if c == b:
+            break
+        c += 1
+
+sie(sieve, c)
